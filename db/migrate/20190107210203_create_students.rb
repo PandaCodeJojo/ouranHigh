6808a482c849ff2d42d2154ee0cd5820e68f48ae
Jodi <jbrowne@nyc.yearup.org>
2019-01-07 16:03:16 -0500

    students and user models

---
+++ db/migrate/20190107210203_create_students.rb
@@ -0,0 +1,13 @@
+class CreateStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :cohort
+      t.integer :age
+      t.string :highest_education_completed
+
+      t.timestamps
+    end
+  end
+end
