813eef2a0eac56be99960282ab6219e434ec0b5f
Jodi <jbrowne@nyc.yearup.org>
2019-01-09 15:57:29 -0500

    fixed UsersController params def

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < Clearance::UsersController
     
     private
     def user_params
-        params.require(user).permit(:username, :email, :password)
+        params.require(:user).permit(:username, :email, :password)
     end
 end
